80927633d763c26afc09d519de45995edb47daf3
Nicholas Alexi <nlas.alexi@gmail.com>
2019-07-28 14:11:02 -0300

    Estudo POO

---
+++ EstudosUm/EstudosUm/Funcoes/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudosUm.Funcoes
+{
+    class Animal
+    {
+        string nome { get; set; }
+
+        public void Acordar()
+        {
+
+            Console.WriteLine("acordei!");
+        }
+        public void Comer()
+        {
+            Console.WriteLine("Vou comer!");
+        }
+        public void Dormir()
+        {
+            Console.WriteLine("vou dormir");
+        }
+    }
+}
